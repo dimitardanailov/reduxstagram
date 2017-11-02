7a0d477b08042f9d22ce9600fe1f703cbd620851
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-11-02 14:23:16 +0200

    Learn Redux #15 - Updating Comment State in our Store

--- client/components/Single.js
+++ client/components/Single.js
@@ -15,7 +15,7 @@ const Single = React.createClass({
 			<div className="single-photo">
 				<Photo i={i} post={post} {...this.props} />
 
-				<Comments postComments={postComments} />
+				<Comments postComments={postComments} {...this.props} />
 			</div>
 		);
 	}
