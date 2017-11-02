2acd9d7af9b797d2801adfcb16008c959144977b
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-11-02 15:03:20 +0200

    Learn Redux #16 - Redux Reducer Composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -3,8 +3,37 @@
 // 1. the action  (info about what happened)
 // 2. copy of current state
 
-function comments(state = [], action) {
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			return [...state, {
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			// we need to return the new state without the deleted comment
+			return [
+				// from the start to the one we want to delete
+				...state.slice(0, action.i),
+				// after the deleted one, to the end
+				...state.slice(action.i + 1)
+			];
+		default:
+			return state;
+	}
 
+	return state;
+}
+
+function comments(state = [], action) {
+	if (typeof action.postId !== 'undefined') {
+		return {
+			// take the current state
+			...state,
+			// overwrite this post with a new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
 
 	return state; 
 }
